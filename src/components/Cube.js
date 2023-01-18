40f0594148755ff0a361a960ea957a2f76f7df75
Jorge G <jorgegomezweb@gmail.com>
2023-01-18 13:42:15 +0100

    feat: add basic structure

---
+++ src/components/Cube.js
@@ -0,0 +1,23 @@
+import { useBox } from "@react-three/cannon";
+
+const Cube = ({ position, args, color, wireframe = false, ...props }) => {
+  /* Physics
+    const [ref] = useBox(() => ({
+      type: 'Static',
+      mass: 1,
+      position,
+      args,
+      ...props,
+    }));
+    */
+
+  // Add ref={ref} to enable physics
+  return (
+    <mesh castShadow position={position}>
+      <boxGeometry args={args} />
+      <meshStandardMaterial color={color} wireframe={wireframe} />
+    </mesh>
+  );
+};
+
+export default Cube;
---
+++ src/components/Floor.js
@@ -0,0 +1,22 @@
+import { usePlane } from "@react-three/cannon";
+
+const Floor = ({ rotation, color, ...props }) => {
+  /* Physics
+    const [ref] = usePlane(() => ({
+      type: 'Static',
+      mass: 0,
+      rotation,
+      ...props,
+    }));
+    */
+
+  // Add ref={ref} to enable physics
+  return (
+    <mesh receiveShadow rotation={rotation}>
+      <planeGeometry args={[1000, 1000]} />
+      <meshStandardMaterial color={color} />
+    </mesh>
+  );
+};
+
+export default Floor;
